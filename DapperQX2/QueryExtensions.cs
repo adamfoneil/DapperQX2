4f8fb5e9da69c65bd3c04817213e54a8650691ad
adamfoneil <adamfoneil@proton.me>
2026-08-22 10:04:52 -0400

    determine param value str within main ext method

--- DapperQX2/QueryExtensions.cs
+++ DapperQX2/QueryExtensions.cs
@@ -12,23 +12,59 @@ public static class QueryExtensions
         string sql, object? parameters, 
         IDbTransaction? txn = null, CommandType? commandType = null, int timeout = 30, string? correlationId = null) =>
         await ExecuteInternalAsync(
-            logger, 
-            nameof(LogQueryAsync), 
-            sql, 
-            parameters, 
-            parameters?.ToString() ?? string.Empty, // todo: build static method and use inside Query class
-            async (s, p, t, ct, to) => await connection.QueryAsync<TResult>(s, p, t, to, ct), 
-            correlationId: correlationId, 
-            txn: txn, 
-            commandType: commandType, 
-            timeout: timeout);
+            logger,
+            nameof(LogQueryAsync),
+            sql,
+            parameters,
+            async (s, p, t, ct, to) => await connection.QueryAsync<TResult>(s, p, t, to, ct),
+            correlationId,
+            txn,
+            commandType,
+            timeout);
+
+    public static async Task<TResult?> LogQuerySingleOrDefaultAsync<TResult, TLogger>(
+        this IDbConnection connection, ILogger<TLogger> logger,
+        string sql, object? parameters,
+        IDbTransaction? txn = null, CommandType? commandType = null, int timeout = 30, string? correlationId = null) =>
+        await ExecuteInternalAsync(
+            logger,
+            nameof(LogQueryAsync),
+            sql,
+            parameters,
+            async (s, p, t, ct, to) => await connection.QuerySingleOrDefaultAsync<TResult>(s, p, t, to, ct),
+            correlationId,
+            txn,
+            commandType,
+            timeout);
+
+    public static string GetParameterString(object? parameters)
+    {
+        if (parameters == null) return "<no parametrers>";
+        
+        if (parameters is IDictionary<string, object> dict)
+        {
+            return string.Join(", ", dict.Select(kvp => $"{kvp.Key}={kvp.Value}"));
+        }
+
+        if (parameters is DynamicParameters dp)
+        {
+            return string.Join(", ", dp.ParameterNames.Select(p => $"{p}={dp.Get<object>(p)}"));
+        }
+        
+        var props = parameters.GetType().GetProperties();
+        if (props.Length == 0) return parameters.ToString() ?? string.Empty;
+        
+        return string.Join(", ", props.Select(p => $"{p.Name}={p.GetValue(parameters)}"));
+    }
 
     internal static async Task<TResult> ExecuteInternalAsync<TResult, TQuery>(
-        ILogger<TQuery> logger,    
-        string queryType, string sql, object? parameters, string paramValueStr,
+        ILogger<TQuery> logger,
+        string queryType, string sql, object? parameters,
         Func<string, object?, IDbTransaction?, CommandType?, int, Task<TResult>> dapperMethod,
         string? correlationId = null, IDbTransaction? txn = null, CommandType? commandType = null, int timeout = 30)
     {
+        var paramValueStr = GetParameterString(parameters);
+
         try
         {
             var sw = Stopwatch.StartNew();
