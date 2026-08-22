0a424d5f36f88b4671db37042f7cf467f0b312d1
adamfoneil <adamfoneil@proton.me>
2026-08-22 07:52:29 -0400

    WhereClause implementation

--- DapperQX2/WhereClause.cs
+++ DapperQX2/WhereClause.cs
@@ -1,10 +1,11 @@
 ﻿using Dapper;
+using System.Text.RegularExpressions;
 
 namespace DapperQX;
 
-public static class WhereClause
+public static partial class WhereClause
 {
-    public record Term(string ParameterName, string Expression, object? Value);
+    public record Term(string Expression, object? Value);
 
     /// <summary>
     /// concatenates multiple expression into a single WHERE clause
@@ -15,10 +16,22 @@ public static class WhereClause
         List<string> useTerms = [];
         foreach (var term in terms.Where(t => t.Value is not null))
         {
-            dp.Add(term.ParameterName, term.Value);
+            var parameters = ExtractParamNames(term.Expression);
+            if (parameters.Length > 1) throw new NotSupportedException("Can't have multiple parameters in expression");
+
+            dp.Add(parameters[0], term.Value);
             useTerms.Add(term.Expression);
         }
 
         return (string.Join(" AND ", useTerms), dp);
     }
+
+    public static string[] ExtractParamNames(string expression)
+    {
+        var regex = ParamRegex();
+        return [..regex.Matches(expression).Select(m => m.Value)];
+    }
+
+    [GeneratedRegex(@"@\w+")]
+    private static partial Regex ParamRegex();
 }
