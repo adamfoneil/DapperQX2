92aacac7532e7a82a854ce7bcf8373f53e762b69
adamfoneil <adamfoneil@proton.me>
2026-08-22 08:52:09 -0400

    not going to use attributes, I think

--- DapperQX2/Attributes/OrderBy.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DapperQX.Attributes;
-
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
-public class OrderByAttribute(object value, string expression) : Attribute
-{
-    public object Value { get; } = value;
-
-    public string Expression { get; } = expression;
-}
--- DapperQX2/Attributes/Top.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DapperQX.Attributes;
-
-/// <summary>
-/// use the {top} token in your query to indicate where to insert the TOP clause
-/// </summary>
-[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
-public class TopAttribute : Attribute
-{
-}
