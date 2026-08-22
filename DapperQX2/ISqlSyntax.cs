278725f8515299cf97daab4d32e57391a540bb80
adamfoneil <adamfoneil@proton.me>
2026-08-22 10:02:25 -0400

    started this but not sure about it

---
+++ DapperQX2/ISqlSyntax.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperQX;
+
+/// <summary>
+/// defines platform-specific SQL syntax elements
+/// </summary>
+public interface ISqlSyntax
+{
+    char LeadDelimiter { get; }
+    char EndDelimiter { get; }
+    string ApplyOffset(string sql, int skip, int take);
+}
